8d7b54ae8963474c741f441082018360a56564fb
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-09-23 15:18:18 +0200

    events

--- test/videohub/videohub.js
+++ test/videohub/videohub.js
@@ -2,19 +2,29 @@ const net = require('net');
 const fs = require('fs');
 
 
-const mode = "routing";
+const mode = "initial";
 const server = net.createServer(function (socket) {
 	let file = fs.readFileSync(`./initial.txt`, 'utf-8');
 	socket.write(file.toString() + '\r\n');
 
 	if (mode != "initial") {
+		console.log("Is not")
 		setInterval(() => {
 			file = fs.readFileSync(`./${mode}.txt`, 'utf-8');
 			socket.write(file.toString() + '\r\n');
 		}, 5000);
 	}
 
-	//socket.pipe(socket);
+	socket.on('data', function(data) {
+		console.log("Data IN:")
+        console.log(data.toString());
+    });
+
+	socket.on('error', function(error){
+		console.log(error);
+	});
+
+	socket.pipe(socket);
 });
 
 server.listen(9990, '127.0.0.1');
