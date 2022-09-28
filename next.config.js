93d43b8bf68fe96da76325ef2e918a42bab573e8
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-09-28 10:11:54 +0200

    dock

--- next.config.js
+++ next.config.js
@@ -12,12 +12,7 @@ cron.schedule('* * * * *', function () {
   console.log('Say scheduled hello')
 });*/
 
-
 module.exports = async (phase) => {
   console.log("Starting at phase:", phase);
-  const videohubs = require('./components/interfaces/videohub/videohubs');
-  await videohubs.loadData()
-  videohubs.connect();
-  console.log("Init done.");
   return nextConfig;
 };
