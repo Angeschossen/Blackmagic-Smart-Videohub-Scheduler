c3e4aa4c990ca7d19cc66665317c85cba82e1b95
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-09-29 14:28:19 +0200

    done

--- backend/server/custom_server.js
+++ backend/server/custom_server.js
@@ -7,14 +7,17 @@ const app = next({ dev: config.parsed.NODE_ENV === "development" })
 const handler = app.getRequestHandler()
 
 app.prepare().then(async _res => {
-    const videohubs = require('./components/interfaces/videohub/videohubs');
-    await videohubs.loadData()
-    videohubs.connect()
+    const videohubs = require('../videohubs');
+    await videohubs.loadData();
+    videohubs.connect();
 
     const server = express()
     server.all('*', handler)
 
-    server.listen(3000)
+    const port = config.parsed.PORT;
+    server.listen(port);
+    console.log(`Listening on port ${port}.`);
+
 }).catch(error => {
     console.log(error);
 });
