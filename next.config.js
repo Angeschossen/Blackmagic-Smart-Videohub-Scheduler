88c5ec8bf64175109a82d0abb9db43a31275cec1
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-09-29 14:40:43 +0200

    c

--- next.config.js
+++ next.config.js
@@ -7,5 +7,12 @@ const nextConfig = {
 
 module.exports = async (phase) => {
   console.log("Starting at phase:", phase);
+
+  if (phase === "phase-production-server" || phase === "phase-development-server") {
+    const videohubs = require('./backend/videohubs');
+    await videohubs.loadData();
+    videohubs.connect();
+  }
+
   return nextConfig;
 };
