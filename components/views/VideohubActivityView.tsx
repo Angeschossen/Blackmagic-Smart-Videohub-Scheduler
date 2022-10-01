5c7f247bd98200177c496efc8c4bf83a64add5cf
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-10-01 23:23:19 +0200

    key

--- components/views/VideohubActivityView.tsx
+++ components/views/VideohubActivityView.tsx
@@ -18,19 +18,20 @@ export const VideohubActivityView = (p: { activityItems: VideohubActivity[] }) =
     <Stack style={videohubPageStyle}>
       <h1>Recent Activity of Videohubs</h1>
       <Stack>
-      {p.activityItems.map((item, _key) => (
-        <ActivityItem activityIcon={<Icon iconName={item.icon} />} timeStamp={item.time.toLocaleDateString() + " " + item.time.toLocaleTimeString()} 
-        comments={[
-          <Link
-            onClick={(event: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement | HTMLElement>) => {
-              Router.push({
-                pathname: '../videohub/main',
-                query: { videohub: item.videohub_id },
-              });
-            }}
-          >{item.title}</Link>,
-        ]} activityDescription={[<Text className={classNames.nameText}>{item.description}</Text>]} key={item.id} className={classNames.exampleRoot} />
-      ))}
+        {p.activityItems.map((item, _key) => (
+          <ActivityItem activityIcon={<Icon iconName={item.icon} />} timeStamp={item.time.toLocaleDateString() + " " + item.time.toLocaleTimeString()}
+            comments={[
+              <Link
+                key={item.id + "_videohub"}
+                onClick={(event: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement | HTMLElement>) => {
+                  Router.push({
+                    pathname: '../videohub/main',
+                    query: { videohub: item.videohub_id },
+                  });
+                }}
+              >{item.title}</Link>,
+            ]} activityDescription={[<Text key={item.id + "_text"} className={classNames.nameText}>{item.description}</Text>]} key={item.id} className={classNames.exampleRoot} />
+        ))}
       </Stack>
     </Stack>
   );
