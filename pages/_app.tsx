cf64cc542e191cbaf20632ba523cd90c8ca47d68
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-10-03 19:38:11 +0200

    session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,21 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { initializeIcons } from '@fluentui/react';
 import React from 'react';
+import { SessionProvider } from "next-auth/react"
+import { Session } from 'next-auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
-
+export default function App({ Component, pageProps }: AppProps<{
+  session: Session
+}>) {
   React.useEffect(() => {
     initializeIcons();
   }, []);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
-
-export default MyApp
