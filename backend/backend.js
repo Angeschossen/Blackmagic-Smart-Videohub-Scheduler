59dd9e29dec0f092784520b405d9d622ca0aa909
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-10-05 13:12:39 +0200

    roles

--- backend/backend.js
+++ backend/backend.js
@@ -12,42 +12,57 @@ class Role {
     }
 }
 
-async function setupRoles() {
-    console.log("Setting up roles...");
-    for (const role of module.exports.roles) {
-        await prisma.client.roles.upsert({
-            where: {
-                id: role.id,
-            },
-            create: {
-                id: role.id,
-                name: role.name,
-                permissions: module.exports.permissions,
-            },
-            update: {
-                name: role.name,
-                permissions: module.exports.permissions,
-            }
-        });
-    }
-    console.log("Roles setup.");
-}
-
 module.exports = {
-    setup: async function () {
-        await setupRoles();
+    permissions: [],
+    roles: [],
+    setupRoles: async function () {
+        console.log("Setting up roles...");
+        permissions = [
+            "videohub_output_schedule",
+            "videohub_pushbuttons_edit"
+        ];
 
-        const videohubs = require('./backend/videohubs');
+        roles = [
+            new Role(1, "Superuser", this.permissions),
+            new Role(2, "Administrator", this.permissions),
+            new Role(3, "User", this.permissions),
+        ];
+
+        await prisma.client.role.deleteMany(); // delete all because of permissions
+        for (const role of roles) {
+            if (await prisma.client.role.findUnique({
+                where: {
+                    id: role.id,
+                }
+            }) == undefined) {
+                await prisma.client.role.upsert({
+                    where: {
+                        id: role.id,
+                    },
+                    create: {
+                        id: role.id,
+                        name: role.name,
+                    },
+                    update: {
+                        name: role.name,
+                    },
+                });
+
+                await prisma.client.rolePermission.createMany({
+                    data: permissions.map((permission, _key)=>{
+                        return {permission: permission, role_id: role.id};
+                    }),
+                });
+            }
+        }
+
+        console.log("Roles setup.");
+    },
+    setup: async function () {
+        await this.setupRoles();
+
+        const videohubs = require('./videohubs');
         await videohubs.loadData();
         videohubs.connect();
     },
-    permissions: [
-        "videohub_output_schedule",
-        "videohub_pushbuttons_edit"
-    ],
-    roles: [
-        new Role(0, "Superuser", permissions),
-        new Role(1, "Administrator", permissions),
-        new Role(2, "User"),
-    ],
 }
