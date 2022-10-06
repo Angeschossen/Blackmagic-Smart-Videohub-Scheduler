94a37b3b63d8cbbd9f5f1ac8f405834d6d7a5da7
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-10-06 18:20:06 +0200

    api auth

--- components/common/ProtectedPage.tsx
+++ components/common/ProtectedPage.tsx
@@ -1,5 +1,5 @@
-import { Label, Spinner, Stack } from "@fluentui/react";
-import { useSession } from "next-auth/react";
+import { DefaultButton, Label, Spinner, Stack } from "@fluentui/react";
+import { signOut, useSession } from "next-auth/react";
 import Router from "next/router";
 import React, { useEffect } from "react";
 import { LoginPage } from "../auth/Login";
@@ -13,10 +13,20 @@ export function useProtectedSession() {
 }
 export const ProtectedPage = (props: InputProps) => {
     // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
-    const { status } = useSession({ required: true })
+    const { data: session, status } = useSession({ required: true });
 
     if (status === "loading") {
         return <></>
+    }
+
+    const sess: any = session;
+    if (sess.user.role_id == undefined) {
+        return (<Stack style={{ display: 'flex', justifyContent: "center", alignItems: "center", minHeight: '100vh' }}>
+            <h1>You haven't been verified yet.</h1>
+            <DefaultButton
+                onClick={() => signOut()}
+            >Logout and try again.</DefaultButton>
+        </Stack>)
     }
 
     return <>{props.children}</>;
