f17cfdcb023bcccfdac9ba9b58237101771baea9
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2022-10-22 19:58:36 +0200

    per user

--- components/interfaces/User.ts
+++ components/interfaces/User.ts
@@ -15,6 +15,6 @@ export interface Role {
     permissions: string[],
 }
 
-export function hasRoleOutput(role: Role|undefined,videohub: Videohub, output_id: number): boolean {
+export function hasRoleOutput(role: Role | undefined, videohub: Videohub, output_id: number): boolean {
     return role != undefined && role.outputs.find(output => output.videohub_id === videohub.id && output.output_id === output_id) != undefined
 }
